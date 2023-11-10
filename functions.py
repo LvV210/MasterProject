fa96e6f2e330462ed792f243819f29be0e188853
LvV210 <luuk.v.vliet@gmail.com>
2023-11-10 19:42:34 +0100

    Made a function to display dataframes stylish

--- functions.py
+++ functions.py
@@ -1,15 +1,5 @@
 import pandas as pd
-
-# def scientific_notation(dataframe):
-#     # Create a copy of the DataFrame to avoid modifying the original data
-#     formatted_df = dataframe.copy()
-
-#     # Iterate through the columns and format numerical values in scientific notation
-#     for col in formatted_df.columns:
-#         if pd.api.types.is_numeric_dtype(formatted_df[col]):
-#             formatted_df[col] = formatted_df[col].apply(lambda x: f"{x:.2E}")
-
-#     return formatted_df
+from IPython.display import Markdown as md
 
 def scientific_notation(df):
     df2 = df.copy()
@@ -24,3 +14,36 @@ def scientific_notation(df):
                 df2.loc[i,j] = "{:.2E}".format(df2.loc[i,j])
 
     return df2
+
+def display_df(df: pd.DataFrame):
+    """
+    Displays a given dataframe in a nice way with better collumn names and units.
+    """
+    columns = {"R_true": (r"$R_{true}[R_{\odot}]$"),
+                "R_expected": r"$R_{expected}[R_{\odot}]$",
+                "R_true/R_expected": r"$\frac{R_{true}}{R_{expected}}$",
+                "L_true": r"$L_{true}[L_{\odot}]$",
+                "L_expected": r"$L_{expected}[L_{\odot}]$",
+                "L_true/L_expected": r"$\frac{L_{true}}{L_{expected}$}",
+                "ST": "Spec. Type",
+                "ST_short": "Spec. Type (short)",
+                "period": r"$P[units]$",
+                "spinperiod": r"$P_{spin}[units]$",
+                "eclipseduration": r"eclipseduration[units]$",
+                "RV": r"$v_{rad}[km s^{-1}]$",
+                "Mob": r"$M_{ob}[M_{\odot}]$",
+                "Mx": r"$M_{x} [M_{\odot}]$",
+                "Rob": r"$R_{ob}[R_{\odot}]$",
+                "Rx": r"$R_{x}[R_{\odot}]$",
+                "parallax": r"$p'' ['']$",
+                "errparallax": r"$\delta p''['']$",
+                "distance": r"$d[kpc]$",
+                "distanceBJ": r"$d_{BJ}[kpc]$",
+                "luminosity": r"$L_{ob}[L_{\odot}]$",
+                "(B-V)obs": r"$(B-V)_{obs}$",
+                "(B-V)0": r"$(B-V)_{0}$",
+                "BC": r"$BC_{v}$",
+                "mv": r"$m_{v}$",
+                "Teff": r"$T_{eff}[K]$"}
+
+    return md(df.rename(columns=columns).to_markdown(index=False))
