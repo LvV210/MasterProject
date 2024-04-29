560fcc2c67b413f901ab70e78e9c35a8cdd7f4a5
LvV210 <luuk.v.vliet@gmail.com>
2024-04-29 12:00:55 +0200

    The luminosity is also calculated via (B-V)\

--- ModelFitting/GridSearchAll.py
+++ ModelFitting/GridSearchAll.py
@@ -8,14 +8,11 @@ from AllModelFunctions import *
 from functions import import_spectra
 
 
-object_names = ['Vela X-1', 'Cen X-3', '4U1538-52', '4U1700-37']
-# 'SMC X-1', 'LMC X-4', 
+object_names = ['SMC X-1', 'LMC X-4', 'Vela X-1', 'Cen X-3', '4U1538-52', '4U1700-37']
 # object_names = ['Vela X-1']
-object_saves = ['VelaX_1', 'CenX_3', '4U1538_52', '4U1700_37']
-# 'SMCX_1', 'LMCX_4'
+object_saves = ['SMCX_1', 'LMCX_4', 'VelaX_1', 'CenX_3', '4U1538_52', '4U1700_37']
 # object_saves = ['VelaX_1']
-galaxies = ['Milkyway', 'Milkyway', 'Milkyway', 'Milkyway']
-# 'SMC', 'LMC', 
+galaxies = ['SMC', 'LMC', 'Milkyway', 'Milkyway', 'Milkyway', 'Milkyway']
 # galaxies = ['Milkyway']
 
 
