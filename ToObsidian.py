834107cebc666991e6e0299bd90c2d175662d167
LvV210 <luuk.v.vliet@gmail.com>
2024-07-30 14:35:13 +0200

    Update

---
+++ ToObsidian.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+# Specify the path of the source file and the destination directory
+objects = ['4U1538_52', '4U1700_37', 'CenX_3', 'LMCX_4', 'SMCX_1', 'VelaX_1']
+source_folder = '/mnt/c/Users/luukv/Documenten/NatuurSterrkenkundeMasterProject/CodeMP/MasterProject/ModelFitting/FitResults/'
+destination_folder = '/mnt/c/Users/luukv/OneDrive/Masterproject/AllNotes/Results/'
+
+# Get all folders in fitresults
+result_folders = folders = [entry for entry in os.listdir(source_folder) if os.path.isdir(os.path.join(source_folder, entry))]
+
+for HMXB in objects:
+    for result_folder in result_folders:
+        if HMXB in result_folder:
+            # print(os.path.join(destination_folder, HMXB), '\n',os.path.join(source_folder, result_folder))
+            print('\n\n')
+            # shutil.copy(result_folder, os.path.join(destination_folder, HMXB))
+
